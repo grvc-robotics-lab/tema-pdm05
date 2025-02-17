28b2b673354cccbce869800c4aaa0e676c39846e
Abdalraheem Ijjeh <abdalraheem.ijjeh@gmail.com>
2025-02-17 14:35:48 +0100

    Update logging_config.py

--- logging_config.py
+++ logging_config.py
@@ -5,7 +5,7 @@ import logging
 logging.basicConfig(
     filename='app_routes.log',
     filemode='w',
-    level=logging.DEBUG,
+    level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
 )
